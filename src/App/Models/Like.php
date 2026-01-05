9d27c7c13be5f0446853e9a84dba5fc57b8a18bc
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-05 19:59:50 +0100

    Update Like Model

--- src/App/Models/Like.php
+++ src/App/Models/Like.php
@@ -22,16 +22,5 @@ class Like{
     {
         return $this->{$name} ?? null;
     }
-
-    public static function is_liked_by(int $reader_id, int $target_id){
-        $db = new Database() ;
-        $stmt = $db->prepare("SELECT * FROM likes WHERE reader_id = :reader_id AND (article_id = :article_id OR comment_id = :comment_id)");
-        $status = $stmt->execute(["reader_id" => $reader_id, "article_id" => $target_id, "comment_id"=> $target_id]);
-        if($status){
-            if($stmt->rowCount() > 0){
-                return true;
-            }
-        }
-        return false;
-    }
+    
 }
