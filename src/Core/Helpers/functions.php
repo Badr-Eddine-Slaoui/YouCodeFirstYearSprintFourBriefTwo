b76405c0a204b77ae3e9858c4b746748ef134cd4
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-05 04:29:51 +0100

    Add Helper Function

---
+++ src/Core/Helpers/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+use Core\Helpers\Auth;
+use Core\Helpers\Request;
+use Core\Helpers\Session;
+use Core\Router\Router;
+
+function session(){
+    return new Session() ;
+}
+
+function auth(){
+    return new Auth() ;
+}
+
+function route(string $name){
+    $router = Router::$instance;
+    return $router->namedRoutes[$name];
+}
+
+function diffForHuman(DateTime $date){
+    $now = new DateTime();
+    $diff = $now->diff($date);
+
+    if ($diff->y > 0) {
+        $comment_diff = $diff->y . ' year' . ($diff->y > 1 ? 's' : '') . ' ago';
+    } elseif ($diff->m > 0) {
+        $comment_diff = $diff->m . ' month' . ($diff->m > 1 ? 's' : '') . ' ago';
+    } elseif ($diff->d > 0) {
+        $comment_diff = $diff->d . ' day' . ($diff->d > 1 ? 's' : '') . ' ago';
+    } elseif ($diff->h > 0) {
+        $comment_diff = $diff->h . ' hour' . ($diff->h > 1 ? 's' : '') . ' ago';
+    } elseif ($diff->i > 0) {
+        $comment_diff = $diff->i . ' min ago';
+    } else {
+        $comment_diff = 'Just now';
+    }
+
+    return $comment_diff;
+}
