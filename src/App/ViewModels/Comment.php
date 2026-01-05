d8f4145b22cc5de67f9c0756fc4144c1a514c3d8
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-05 19:53:39 +0100

    Add Comment View Model

---
+++ src/App/ViewModels/Comment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\ViewModels;
+
+use App\Models\Reader;
+use DateTime;
+
+class Comment{
+    private int $id;
+    private int $article_id;
+    private int $reader_id;
+    private string $body;
+    private int $likes_count;
+    private DateTime $created_at;
+    private Reader $reader;
+    private bool $is_liked_by_current_user;
+
+    public function __construct(int $id, int $article_id, int $reader_id, string $body, string $created_at, Reader $reader, bool $is_liked_by_current_user, int $likes_count = 0){
+        $this->id = $id;
+        $this->article_id = $article_id;
+        $this->reader_id = $reader_id;
+        $this->body = $body;
+        $this->created_at = new DateTime($created_at);
+        $this->likes_count = $likes_count;
+        $this->reader = $reader;
+        $this->is_liked_by_current_user = $is_liked_by_current_user;
+    }
+
+    public function __get($name)
+    {
+        return $this->{$name} ?? null;
+    }
+}
