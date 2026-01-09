c20e27f544762816019d0f54a43227c4ee4ff74e
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:33:06 +0100

    Update Comment Repositoty

--- src/App/Repositories/CommentRepository.php
+++ src/App/Repositories/CommentRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repositories;
 
 use App\DAOs\CommentDAO;
+use App\DAOs\ReaderDAO;
+use App\DAOs\ReportDAO;
 use App\Mappers\CommentMapper;
+use App\Mappers\ReaderMapper;
 
 class CommentRepository{
     private static ?CommentRepository $instance = null;
@@ -20,12 +23,22 @@ class CommentRepository{
 
     public function getAll(): ?array{
         $commentDAO = CommentDAO::getInstance();
+        $readerDAO= ReaderDAO::getInstance();
+        $articleRepository = ArticleRepository::getInstance();
+        $readerMapper= ReaderMapper::getInstance();
         $commentMapper= CommentMapper::getInstance();
 
         $rows = $commentDAO->getAll();
 
         if($rows){
-            return $commentMapper->mapAll($rows);
+            foreach($rows as $key => $comment){
+                $reader = $readerDAO->findById($comment['reader_id']); 
+                $comment['reader'] = $readerMapper->map($reader);
+                $comment['article'] = $articleRepository->findById($comment['article_id']);
+                $rows[$key] = $comment;
+            }
+
+            return $commentMapper->toCommentsView($rows);
         }
 
         return null;
@@ -34,6 +47,42 @@ class CommentRepository{
     public function getByArticle(int $articleId): ?array{
         $commentDAO = CommentDAO::getInstance();
         return $commentDAO->getByArticleId($articleId);
+    }
+
+    public function getByArticleAuthor(int $authorId): ?array{
+        $commentDAO = CommentDAO::getInstance();
+        $readerDAO = ReaderDAO::getInstance();
+        $reportDAO = ReportDAO::getInstance();
+        $articleRepository = ArticleRepository::getInstance();
+        $readerMapper= ReaderMapper::getInstance();
+        $commentMapper= CommentMapper::getInstance();
+
+        $commentsData = $commentDAO->getByArticleAuthor($authorId);
+
+        if($commentsData){
+            foreach($commentsData as $key => $comment){
+                $is_reported = $reportDAO->isReportedBy(session()->get('user_id'), $comment['id']);
+                $comment['is_reported_by_current_user'] = $is_reported;
+                $reader = $readerDAO->findById($comment['reader_id']); 
+                $comment['reader'] = $readerMapper->map($reader);
+                $comment['article'] = $articleRepository->findById($comment['article_id']);
+                $commentsData[$key] = $comment;
+            }
+
+            return $commentMapper->toCommentsView($commentsData);
+        }
+
+        return null;
+    }
+
+    public function getAuthorCommentsCount(int $authorId): ?int{
+        $commentDAO = CommentDAO::getInstance();
+        return $commentDAO->getAuthorCommentsCount($authorId);
+    }
+
+    public function getAuthorDailyCommentsCount(int $authorId): ?int{
+        $commentDAO = CommentDAO::getInstance();
+        return $commentDAO->getAuthorDailyCommentsCount($authorId);
     }
 
     public function create(array $data): bool{
