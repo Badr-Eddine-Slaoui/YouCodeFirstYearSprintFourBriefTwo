18f547ff16ad96fa8e0ed04aad7aea33e9ae60c7
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:31:07 +0100

    Add Report Mapper

---
+++ src/App/Mappers/ReportMapper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mappers;
+
+use App\ViewModels\Report;
+
+class ReportMapper
+{
+    private static ?ReportMapper $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): ReportMapper
+    {
+        if (self::$instance === null) {
+            self::$instance = new ReportMapper();
+        }
+
+        return self::$instance;
+    }
+
+    public function toReportsView(array $reports):array{
+        return array_map(
+            fn($report) => new Report(
+                $report['id'],
+                $report['first_name'],
+                $report['last_name'],
+                $report['content'],
+                $report['reason'],
+                $report['status'],
+                $report['created_at'],
+                $report['type']
+            ),
+            $reports
+        );
+    } 
+}
