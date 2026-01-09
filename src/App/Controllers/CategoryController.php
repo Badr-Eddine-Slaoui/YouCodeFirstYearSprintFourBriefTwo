ebd2cc491a558ee263085dab9831624ac462c52a
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:37:12 +0100

    Update Category Controller

--- src/App/Controllers/CategoryController.php
+++ src/App/Controllers/CategoryController.php
@@ -2,34 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Models\Category;
 use App\Services\CategoryService;
 use Core\Base\Controller;
-use Core\Database\Database;
 use Core\Helpers\Redirect;
 use Core\Helpers\Request;
 use Core\Helpers\Session;
-use Core\Helpers\Validator;
 
 class CategoryController extends Controller
 {
-    public function index()
-    {
-        $service = CategoryService::getInstance();
-
-        $categories = $service->getCategoriesWithArticleCount();
-        $categories_count = count($categories);
-        $unused_categories_count = $service->getUnusedCategoriesCount();
-        $most_used_category_name = $service->getMostUsedCategoryName();
-
-        if($categories && $most_used_category_name){
-            return $this->view('admin.categories.index', compact('categories', 'categories_count', 'unused_categories_count', 'most_used_category_name'),'admin');
-        }
-
-        Session::set("error","Something went wrong, try again later");
-        return Redirect::back();
-    }
-
     public function create()
     {
         return $this->view('admin.categories.create', layout: 'admin');
