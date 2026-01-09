c2744749631c0e44863066ded9f58e1438bd5b62
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:35:42 +0100

    Add Report View Model

---
+++ src/App/ViewModels/Report.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\ViewModels;
+
+use DateTime;
+
+class Report{
+    private int $id;
+    private string $first_name;
+    private string $last_name;
+    private string $content;
+    private string $reason;
+    private string $status;
+    private DateTime $created_at;
+    private string $type;
+
+    public function __construct(int $id, string $first_name, string $last_name, string $content, string $reason, string $status, string $created_at, string $type){
+        $this->id = $id;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->content = $content;
+        $this->reason = $reason;
+        $this->status = $status;
+        $this->created_at = new DateTime($created_at);
+        $this->type = $type;
+    }
+
+    public function __get($name)
+    {
+        return $this->{$name} ?? null;
+    }
+}
