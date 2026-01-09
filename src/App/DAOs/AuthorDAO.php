67761e65779afb9b6157c0d833cc7d3d546dcb4c
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:29:08 +0100

    Update Author DAO

--- src/App/DAOs/AuthorDAO.php
+++ src/App/DAOs/AuthorDAO.php
@@ -31,5 +31,56 @@ class AuthorDAO{
 
         return null;
     }
+
+    public function getAuthorArticlesInteractions(int $id): ?array{
+        $db = Database::getInstance();
+
+        $stmt = $db->prepare("
+            SELECT *
+            FROM (
+                SELECT 
+                    l.id,
+                    u.first_name,
+                    u.last_name,
+                    '' AS body,
+                    l.article_id,
+                    a.author_id,
+                    a.title,
+                    l.created_at AS created_at,
+                    'like' AS type
+                FROM articles a
+                JOIN likes l ON a.id = l.article_id
+                JOIN users u ON u.id = l.reader_id
+                WHERE a.author_id = :id
+
+                UNION ALL
+
+                SELECT 
+                    c.id,
+                    u.first_name,
+                    u.last_name,
+                    c.body,
+                    c.article_id,
+                    a.author_id,
+                    a.title,
+                    c.created_at AS created_at,
+                    'comment' AS type
+                FROM articles a
+                JOIN comments c ON a.id = c.article_id
+                JOIN users u ON u.id = c.reader_id
+                WHERE a.author_id = :id
+            ) AS interactions
+            ORDER BY created_at DESC
+            LIMIT 5;
+        ");
+
+        $status = $stmt->execute(['id' => $id]);
+
+        if($status){
+            return $stmt->fetchAll();
+        }
+
+        return null;
+    }
     
 }
