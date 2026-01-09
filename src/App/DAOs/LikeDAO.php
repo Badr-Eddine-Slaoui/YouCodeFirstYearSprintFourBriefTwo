19af2e5203b9def0596bb0f2b7a0977829d36426
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:29:16 +0100

    Update Author DAO

--- src/App/DAOs/LikeDAO.php
+++ src/App/DAOs/LikeDAO.php
@@ -17,6 +17,59 @@ class LikeDAO{
         return self::$instance;
     }
 
+    public function getByArticleAuthor(int $articleAuthorId): ?array
+    {
+        $db = Database::getInstance();
+
+        $stmt = $db->prepare("SELECT l.*
+            FROM likes l
+            LEFT JOIN articles a ON a.id = l.article_id
+            WHERE a.author_id = :article_author_id
+            ORDER BY l.created_at DESC");
+
+        $status = $stmt->execute(['article_author_id' => $articleAuthorId]);
+
+        if ($status) {
+            return $stmt->fetchAll();
+        }
+
+        return null;
+    }
+
+    public function getAuthorLikesCount(int $authorId): ?int{
+        $db = Database::getInstance();
+
+        $stmt = $db->prepare("SELECT COUNT(l.id) as count FROM likes l LEFT JOIN articles a ON a.id = l.article_id WHERE a.author_id = :author_id");
+
+        $status = $stmt->execute(["author_id"=> $authorId]);
+
+        if ($status) {
+            return (int) $stmt->fetchColumn();
+        }
+
+        return null;
+    }
+
+    public function getAuthorDailyAvgLikesCount(int $authorId): ?int{
+        $db = Database::getInstance();
+
+        $stmt = $db->prepare("SELECT COUNT(l.id) as count 
+                                    FROM likes l 
+                                    LEFT JOIN articles a 
+                                    ON a.id = l.article_id 
+                                    WHERE a.author_id = :author_id 
+                                    AND EXTRACT(WEEK FROM l.created_at) = EXTRACT(WEEK FROM CURRENT_DATE)
+                                    AND EXTRACT(YEAR FROM l.created_at) = EXTRACT(YEAR FROM CURRENT_DATE)");
+
+        $status = $stmt->execute(["author_id"=> $authorId]);
+
+        if ($status) {
+            return (int) $stmt->fetchColumn();
+        }
+
+        return null;
+    }
+
     public function likeArticle(int $articleId, int $userId): bool
     {
         $db = Database::getInstance();
@@ -77,10 +130,10 @@ class LikeDAO{
         ]);
     }
 
-    public function isLikedBy(int $reader_id, int $target_id): bool{
+    public function isLikedBy(int $reader_id, int $target_id, string $target_type): bool{
         $db = Database::getInstance();
-        $stmt = $db->prepare("SELECT * FROM likes WHERE reader_id = :reader_id AND (article_id = :article_id OR comment_id = :comment_id)");
-        $status = $stmt->execute(["reader_id" => $reader_id, "article_id" => $target_id, "comment_id"=> $target_id]);
+        $stmt = $db->prepare("SELECT * FROM likes WHERE reader_id = :reader_id AND {$target_type}_id = :target_id");
+        $status = $stmt->execute(["reader_id" => $reader_id, "target_id" => $target_id]);
         if($status){
             if($stmt->rowCount() > 0){
                 return true;
