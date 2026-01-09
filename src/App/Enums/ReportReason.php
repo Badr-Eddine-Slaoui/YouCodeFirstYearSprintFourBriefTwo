d19f22a457e35970e75afb3712d4276cf45ae40a
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-09 10:29:54 +0100

    Update Report Reason Enum

--- src/App/Enums/ReportReason.php
+++ src/App/Enums/ReportReason.php
@@ -3,11 +3,12 @@
 namespace App\Enums;
 
 enum ReportReason: string {
-    case SPAM = 'spam';
-    case INAPPROPRIATE = 'inappropriate';
-    case OTHER = 'other';
+    case SPAM = "spam";
+    case EXPLICIT_CONTENT = "explicit content";
+    case INSULT = "insult";
+    case RACIST_CONTENT = "racist content";
+    case OFFENSIVE = "offensive";
+    case ABUSE = "abuse";
+    case COPYRIGHT = "copyright";
+    case OTHER = "other";
 }
-
-$reason = 'innapropriate';
-
-ReportReason::SPAM->value === $reason;
