52cb1030ea5b227f533f3451b559115f2a623798
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-11 04:33:10 +0100

    Update Report View Model

--- src/App/ViewModels/Report.php
+++ src/App/ViewModels/Report.php
@@ -6,6 +6,7 @@ use DateTime;
 
 class Report{
     private int $id;
+    private int $user_id;
     private string $first_name;
     private string $last_name;
     private string $content;
@@ -14,8 +15,9 @@ class Report{
     private DateTime $created_at;
     private string $type;
 
-    public function __construct(int $id, string $first_name, string $last_name, string $content, string $reason, string $status, string $created_at, string $type){
+    public function __construct(int $id, int $user_id, string $first_name, string $last_name, string $content, string $reason, string $status, string $created_at, string $type){
         $this->id = $id;
+        $this->user_id = $user_id;
         $this->first_name = $first_name;
         $this->last_name = $last_name;
         $this->content = $content;
