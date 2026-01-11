80a4eda53d1d320a7852ed68b7809cc321b0ed68
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2026-01-11 04:33:36 +0100

    Update Auth Helper

--- src/Core/Helpers/Auth.php
+++ src/Core/Helpers/Auth.php
@@ -7,6 +7,7 @@ use App\Models\Author;
 use App\Models\Reader;
 use App\Models\User;
 use Core\Database\Database;
+use DateTime;
 
 class Auth{
     public static function login(string $email, string $password): bool{
@@ -20,11 +21,30 @@ class Auth{
         $user = $stmt->fetch();
 
         if(empty($user)){
+            Session::flash("error", "Invalid credentials");
             return false;
         }
         
         if(!password_verify($password, $user["password"])){
+            Session::flash("error", "Invalid credentials");
             return false;
+        }
+
+        if($user["is_baned"]){
+            Session::flash("error", "You are banned");
+            return false;
+        }
+
+        if($user["is_blacklisted"]){
+            Session::flash("error", "You are blacklisted");
+            return false;
+        }
+
+        if($user["suspend_until"]){
+            if(new DateTime($user["suspend_until"]) > new DateTime()){
+                Session::flash("error", "You are suspended");
+                return false;
+            }
         }
 
         Session::set("user_id", $user["id"]);
@@ -96,11 +116,11 @@ class Auth{
         if($user){
 
             if($user["role"] === "admin"){
-                $user = new Admin($user["first_name"], $user["last_name"], $user["email"], $user["password"], $user["created_at"]);
+                $user = new Admin((int) $user["id"], $user["first_name"], $user["last_name"], $user["email"], $user["password"], $user["is_baned"], $user["is_blacklisted"], $user["suspend_until"], $user["timeouted_until"], $user["created_at"]);
             }else if($user["role"] === "author") {
-                $user = new Author($user["first_name"], $user["last_name"], $user["email"], $user["password"], $user["created_at"]);
+                $user = new Author((int) $user["id"], $user["first_name"], $user["last_name"], $user["email"], $user["password"], $user["is_baned"], $user["is_blacklisted"], $user["suspend_until"], $user["timeouted_until"], $user["created_at"]);
             }else{
-                $user = new Reader($user["first_name"], $user["last_name"], $user["email"], $user["password"], $user["created_at"]);
+                $user = new Reader((int) $user["id"], $user["first_name"], $user["last_name"], $user["email"], $user["password"], $user["is_baned"], $user["is_blacklisted"], $user["suspend_until"], $user["timeouted_until"], $user["created_at"]);
             }
 
             return $user;
